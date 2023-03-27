8af1c6799eeb74f860a52e9f7c12165aa780c03e
jongseo <98136834+belllead@users.noreply.github.com>
2023-03-27 12:29:46 +0900

    [D1] Title: 스탬프 찍기, Time: 140 ms, Memory: 21,172 KB -BaekjoonHub

--- SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class Solution {
+
 	public static void main(String[] args) {
-		// 1. 입력 받기
+		
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		// 2. (입력받은 숫자의 개수만큼) 출력하기
-		for(int i = 0; i < num; i++) {
+		int N = sc.nextInt();
+		
+		for (int i=0; i<N; i++)
 			System.out.print("#");
-		}
+		
 	}
+	
 }
