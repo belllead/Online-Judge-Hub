9dea62fafa6d3debf7bd35ec0141667728f37eb4
jongseo <98136834+belllead@users.noreply.github.com>
2023-03-27 11:54:24 +0900

    [Silver IV] Title: 동전 0, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] coin = new int[N];
+		for (int i=0; i<N; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int cnt = 0;
+		
+		for (int i=N-1; i>=0; i--) {
+			if (K >= coin[i]) {
+				cnt += K / coin[i];
+				K = K % coin[i];
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
