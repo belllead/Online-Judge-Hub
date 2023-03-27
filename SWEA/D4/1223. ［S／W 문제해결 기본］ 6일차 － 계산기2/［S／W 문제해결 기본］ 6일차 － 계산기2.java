b91200c74e435c9f26fee5d554426d9d29b34c25
jongseo <98136834+belllead@users.noreply.github.com>
2023-03-27 14:13:44 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 141 ms, Memory: 20,808 KB -BaekjoonHub

---
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+	static Stack<Character> s1 = new Stack<>();
+	static Stack<Integer> s2 = new Stack<>();
+	static char[] num = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		for (int tc=1; tc<=10; tc++) {
+			int N = sc.nextInt();
+			String s = sc.next();
+			
+			char[] inArr = s.toCharArray();
+//			System.out.println(inArr);
+			
+			char[] sufArr = toSufOp(inArr);
+//			System.out.println(sufArr);
+			
+			System.out.println("#" + tc + " " + calculate(sufArr));
+		}
+	}
+	
+	static int calculate(char[] sufArr) {
+		s2.clear();
+		
+		for (char c : sufArr) {
+			if (checkInt(c))
+				s2.push(c - '0');
+			else {
+				if (c == '*') {
+					s2.push(s2.pop() * s2.pop());
+				} else {
+					s2.push(s2.pop() + s2.pop());
+				}
+			}
+//			System.out.println(s2.toString());
+		}
+		return s2.pop();
+	}
+	
+	static char[] toSufOp(char[] inArr) {
+		s1.clear();
+		
+		char[] sufArr = new char[inArr.length];
+		
+		int sufIdx = 0;
+		
+		for (char c : inArr) {
+			if (checkInt(c))
+				sufArr[sufIdx++] = c;	
+			else {
+				if (s1.isEmpty()) {
+					s1.push(c);
+				} else if (c == '+') {
+					sufArr[sufIdx++] = s1.pop();
+					s1.push(c);
+				} else {
+					if (s1.peek() == '+')
+						s1.push(c);
+					else {
+						sufArr[sufIdx++] = s1.pop();
+						s1.push(c);
+					}						
+				}
+			}
+//			System.out.println(s1.toString());
+		}
+		sufArr[sufIdx] = s1.pop();		
+		
+		return sufArr;
+	}
+	
+	static boolean checkInt(char c) {
+		for (char n : num) {
+			if (c == n)
+				return true;
+		}
+		return false;
+	}
+}
