0610f2ecf84cbe2b194a64049b8e8d91df6e5955
jongseo <98136834+belllead@users.noreply.github.com>
2023-03-27 11:53:05 +0900

    [Silver V] Title: 덩치, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[N][2];
+		int[] rank = new int[N];
+		
+		for (int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] =  Integer.parseInt(st.nextToken());
+			arr[i][1] =  Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank[i]++;
+				}
+			}			
+		}
+		
+		for (int i=0; i<N; i++) {
+			System.out.print(++rank[i] + " ");
+		}
+	}
+}
