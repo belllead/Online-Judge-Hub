aa08fb003612bba99890b8b7eee9edf716b9954f
jongseo <98136834+belllead@users.noreply.github.com>
2023-03-27 12:36:48 +0900

    [D2] Title: 달팽이 숫자, Time: 137 ms, Memory: 20,644 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Solution {
+	static int cnt, N;
+	static int[][] snail;
+	static StringBuilder sb = new StringBuilder();	
+	
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int testCase=0; testCase<T; testCase++) {
+			N = sc.nextInt();
+			snail = new int[N][N];
+			
+			sb.append("#" + (testCase+1) + "\n");
+			
+			snailNum(0, 0, N-1, N-1, 1);
+		}
+		System.out.println(sb.toString());
+						
+	}
+	
+	public static void snailNum(int startRow, int startCol, int endRow, int endCol, int startCnt) {			
+		cnt = startCnt;
+		
+		for (int c=startCol; c<=endCol; c++) {
+			snail[startRow][c] = cnt++;
+		}
+		
+		for (int r=startRow+1; r<=endRow; r++) {
+			snail[r][endCol] = cnt++;
+		}
+		
+		for (int c=endCol-1; c>=startCol; c--) {
+			snail[endRow][c] = cnt++;
+		}
+		
+		for (int r=endRow-1; r>=startRow+1; r--) {
+			snail[r][startCol] = cnt++;
+		}
+		
+		if (cnt == N * N + 1) {
+			for (int r=0; r<N; r++) {
+				for (int c=0; c<N; c++) {
+					sb.append(snail[r][c] + " ");
+				}
+				sb.append("\n");
+			}
+			return;
+		} else {
+			snailNum(startRow+1, startCol+1, endRow-1, endCol-1, cnt);
+		}
+			
+	}
+	
+}
