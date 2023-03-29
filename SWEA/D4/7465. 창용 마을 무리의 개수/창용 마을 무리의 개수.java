27512bf7a3af4b78667df39ad17d4f43b6c10881
jongseo <98136834+belllead@users.noreply.github.com>
2023-03-29 15:59:46 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 147 ms, Memory: 26,964 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
