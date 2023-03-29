fdf842e3a4d0d871b8b00b981a29cb403ab17029
jongseopark <sociojs85+dev@gmail.com>
2023-03-29 12:22:58 +0900

    manual modification2

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,68 +1,93 @@
-import java.io.*;
-import java.util.*;
+package baeckjoon.gold;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer VE = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(VE.nextToken());
-        int E = Integer.parseInt(VE.nextToken());
-        int start = Integer.parseInt(br.readLine());
-        
-        ArrayList<edge>[] graph = new ArrayList[V+1];
-        for (int i = 0; i <= V; i++)
-            graph[i] = new ArrayList<>();
-        
-        for (int i = 0; i < E; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[s].add(new edge(e, w));
-        }
-        
-        int[] distance = new int[V+1];
-        boolean[] visited = new boolean[V+1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[start] = 0;
-        
-        PriorityQueue<edge> pq = new PriorityQueue<>();
-        pq.add(new edge(start, 0));
-        
-        while (!pq.isEmpty()) {
-            edge cur = pq.poll();
-            
-            if (visited[cur.e]) continue;
-            visited[cur.e] = true;
-            
-            for (edge next : graph[cur.e]) {
-                if (distance[next.e] > distance[cur.e] + next.w) {
-                    distance[next.e] = distance[cur.e] + next.w;
-                    pq.add(new edge(next.e, distance[next.e]));
-                }
-            }
-        }
-        for (int i = 1; i <= V; i++) {
-            if (distance[i] == Integer.MAX_VALUE) sb.append("INF\n");
-            else sb.append(distance[i]+"\n");
-        }
-//        System.out.println(Arrays.toString(distance));
-        System.out.println(sb);
-    }
-    
-    static class edge implements Comparable<edge>{
-        int e;
-        int w;
-        
-        public edge(int e, int w) {
-            this.e = e;
-            this.w = w;
-        }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-        @Override
-        public int compareTo(edge o) {
-            return this.w - o.w;
-        }
-    }
+public class ShortestWay1753 {
+	
+	static class Edge implements Comparable<Edge> {
+		int target;
+		int weight;
+		
+		public Edge(int target, int weight) {
+			super();
+			this.target = target;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.weight - o.weight;
+		}
+	}
+	
+	static int V, start;
+	static List<Edge>[] adjList; 
+	static int[] distance;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		start = Integer.parseInt(br.readLine());
+		
+		adjList = new ArrayList[V+1];
+		distance = new int[V+1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		distance[start] = 0;
+		
+		for (int i=0; i<V+1; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		
+		while (E-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			adjList[s].add(new Edge(e, w));
+		}
+		
+		dijkstra();
+		
+		for (int i=1; i<V+1; i++) {
+			if (distance[i] == Integer.MAX_VALUE)
+				System.out.println("INF");
+			else
+				System.out.println(distance[i]);
+		}
+	}
+	
+	static void dijkstra() {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		boolean[] visited = new boolean[V+1];
+		
+		pq.offer(new Edge(start, 0));
+		
+		while (!pq.isEmpty()) {
+			Edge curr = pq.poll();
+			
+			if (visited[curr.target]) continue;
+
+			visited[start] = true;
+				
+			for (Edge next : adjList[curr.target]) {
+				
+				if (distance[next.target] > distance[curr.target] + next.weight) {
+					distance[next.target] = distance[curr.target] + next.weight;
+					pq.offer(new Edge(next.target, distance[next.target]));
+				}
+			}
+		}
+	}
 }
+
+
