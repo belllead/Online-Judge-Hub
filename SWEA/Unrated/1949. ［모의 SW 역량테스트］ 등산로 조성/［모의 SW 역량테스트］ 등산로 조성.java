154563e1199d69e7f91828db175dd437a9cafcae
jongseo <98136834+belllead@users.noreply.github.com>
2023-04-06 13:48:47 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 174 ms, Memory: 29,840 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -43,12 +43,6 @@ public class Solution {
 			
 			// 입력완료
 			
-//			for (int i=1; i<=K; i++) {
-//				dig = i;
-//				for (int[] p : peakList)
-//					bfs(p[0], p[1]);
-//			}
-			
 			for (int[] p : peakList) {
 				visited[p[0]][p[1]] = true;
 				dfs(p[0], p[1], 1, 0, mount);
@@ -63,62 +57,7 @@ public class Solution {
 		sc.close();
 	}
 
-//	private static void bfs(int row, int col) {
-//		Queue<int[]> q = new LinkedList<>();
-//		boolean[][][] visited = new boolean[N][N][2];
-//		
-//		q.offer(new int[] {row, col, 0});
-//		visited[row][col][0] = true;
-//		
-//		int cnt = 0;
-//		
-//		while (!q.isEmpty()) {
-//			int size = q.size();
-//		
-//			cnt++;
-//
-//			for (int[] l : q)
-//				System.out.println(Arrays.toString(l));
-//			System.out.println();
-//			while (size-- > 0) {
-//				int[] curr = q.poll();
-//				
-//				for (int i=0; i<4; i++) {
-//					int tr = curr[0] + dr[i];
-//					int tc = curr[1] + dc[i];
-//					int dug = curr[2];
-//					
-//					// 경계 안에서
-//					if (tr >= 0 && tr < N && tc >= 0 && tc < N) {
-//						// 공사한 적이 없다면
-//						if (dug == 0) {
-//							// 공사 안 한 갈래로 보내보고
-//							if (!visited[tr][tc][dug] && mount[tr][tc] < mount[curr[0]][curr[1]]) {
-//								q.offer(new int[] {tr, tc, dug});
-//								visited[tr][tc][dug] = true;
-//							}
-//							
-//							// 공사 하고 보내본다
-//							mount[tr][tc] -= dig;
-//							if (!visited[tr][tc][dug+1] && mount[tr][tc] < mount[curr[0]][curr[1]]) {
-//								q.offer(new int[] {tr, tc, dug+1});
-//								visited[tr][tc][dug+1] = true;
-//							}
-//							mount[tr][tc] += dig;
-//						} else { // 공사한 적이 있다면
-//							if (!visited[tr][tc][dug] && mount[tr][tc] < mount[curr[0]][curr[1]]) {
-//								q.offer(new int[] {tr, tc, dug});
-//								visited[tr][tc][dug] = true;
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//		max=Math.max(max,cnt);
-//	}
-
-	private static void dfs(int row, int col, int cnt, int dug, int[][] mount) {
+    private static void dfs(int row, int col, int cnt, int dug, int[][] mount) {
 		max=Math.max(max, cnt);
 		
 		for (int i=0; i<4; i++) {
