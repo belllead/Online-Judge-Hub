f0a0e1e09bc78ddbeac4be32a11c015d695ea799
jongseo <98136834+belllead@users.noreply.github.com>
2023-06-20 19:10:51 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 123 ms, Memory: 19,876 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,37 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
-		for(int tc=1; tc<=T; tc++) {
+		for (int tc=1; tc<=T; tc++) {
 			int N = Integer.parseInt(br.readLine());
-			int ans = 0;
-			List<String> nums = new ArrayList<>();
+//			System.out.println(N);
+			// 0~9까지 체크할 수 있는 이진수 숫자
+			// 10,000,000,000
+			long check = (1 << 10);
+			int add = N;
 			
-			for(int i=0; i<=9; i++) {
-				nums.add(Integer.toString(i));
-			}
-			
-			while (nums.size() != 0) {
-				ans += N;
-				for(int i=nums.size()-1; i>=0; i--) {
-					if (Integer.toString(ans).contains(nums.get(i))) {
-						nums.remove(i);
-					}
+			// 0~9까지 체크되면 break
+			while (true) {
+				int digits = N;
+				
+				while (digits >= 1) {
+//					System.out.println(digits);
+					int digit = digits % 10;
+					check = check | (1 << digit);
+					digits /= 10;
 				}
-			
-			}	
-			System.out.println("#" + tc + " " + ans);
+//				System.out.println("check: " + Long.toBinaryString(check));
+				if ((((1 << 11) - 1) & check) == ((1 << 11) - 1)) {
+					sb.append("#").append(tc).append(" ").append(N).append("\n");
+					break;
+				}
+				
+				N += add;
+//				System.out.println(N);
+			}
 		}
-		
-		
-		
+		System.out.println(sb);
 	}
+	
 }
